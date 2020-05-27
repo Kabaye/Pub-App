4704928ce94001110885b900747702aae3f15abf
svku0919 <svkulich@gmail.com>
2020-05-27 18:36:46 +0300

    feat: add Storage microservice

---
+++ storage-app/src/main/java/by/pub/storage/app/StorageAppApplication.java
@@ -0,0 +1,13 @@
+package by.pub.storage.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StorageAppApplication.class, args);
+    }
+
+}
---
+++ storage-app/src/test/java/by/pub/storage/app/StorageAppApplicationTests.java
@@ -0,0 +1,13 @@
+package by.pub.storage.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
