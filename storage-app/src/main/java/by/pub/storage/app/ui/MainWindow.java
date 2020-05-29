d303b2b55596f0592213ae8a4608ed536e6f3e7d
Yauhen Mardan <yauhenmardan@gmail.com>
2020-05-29 09:53:57 +0300

    feat: add basic editing abilities for IngredientRequestJList

--- storage-app/src/main/java/by/pub/storage/app/ui/MainWindow.java
+++ storage-app/src/main/java/by/pub/storage/app/ui/MainWindow.java
@@ -9,12 +9,14 @@ import java.awt.GridLayout;
 import javax.swing.BorderFactory;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.ListModel;
+import javax.swing.ListSelectionModel;
 import javax.swing.SwingConstants;
 import javax.swing.WindowConstants;
 import org.springframework.stereotype.Component;
@@ -22,23 +24,25 @@ import org.springframework.stereotype.Component;
 @Component
 public class MainWindow extends JFrame {
 
-    private static final Font HEADER_FONT = new Font("Serif", Font.PLAIN, 30);
+    private final JPanel ingredientPanel;
 
+    private static final Font HEADER_FONT = new Font("Serif", Font.PLAIN, 30);
     private static final int SCREEN_WIDTH = 1000;
     private static final int SCREEN_HEIGHT = 600;
-    private final JPanel ingredientPanel;
 
     private final JPanel mainPanel;
     private final JPanel authPanel;
+    private final JScrollPane ingredientScrollPane;
     private final JPanel ingredientRequestPanel;
+    private final JScrollPane ingredientRequestScrollPane;
+    private final ListModel<IngredientRequest> ingredientRequestListModel;
     private final JLabel ingredientLabel;
     private final JLabel ingredientRequestLabel;
-    private final JScrollPane ingredientScrollPane;
-    private final JScrollPane ingredientRequestScrollPane;
     private final ListModel<Ingredient> ingredientListModel;
+    private final JButton fulfillButton;
     private final JList<Ingredient> ingredientJList;
     private final JList<IngredientRequest> ingredientRequestJList;
-    private final ListModel<IngredientRequest> ingredientRequestListModel;
+    private final JButton ingredientRequestButton;
 
     public MainWindow() {
         authPanel = new JPanel();
@@ -59,19 +63,36 @@ public class MainWindow extends JFrame {
         ingredientRequestButton = new JButton("Request ingredients");
         ingredientLabel = new JLabel("Available ingredients");
 
+        addListeners();
         addComponentsToIngredientPanel();
         addComponentsToIngredientRequestPanel();
         addComponentsToMainPanel();
         configureComponents();
-        setMainWindowPreferences();
-    }
+        setWindowPreferences();
 
-    private final JButton fulfillButton;
-    private final JButton ingredientRequestButton;
+        addIngredientRequest(new IngredientRequest());
+        addIngredientRequest(new IngredientRequest());
+        addIngredientRequest(new IngredientRequest());
+        addIngredientRequest(new IngredientRequest());
+    }
 
     public static void main(String[] args) {
         MainWindow window = new MainWindow();
         window.setVisible(true);
+    }
+
+    private void addListeners() {
+        fulfillButton.addActionListener(e -> {
+            ListSelectionModel selectionModel = ingredientRequestJList.getSelectionModel();
+            int index = selectionModel.getMinSelectionIndex();
+            if (index >= 0) {
+                removeIngredientRequest(index);
+            }
+        });
+        ingredientRequestButton.addActionListener(e -> {
+            JDialog dialog = new RequestProviderDialog(MainWindow.this);
+            dialog.setVisible(true);
+        });
     }
 
     private void addComponentsToIngredientPanel() {
@@ -98,6 +119,7 @@ public class MainWindow extends JFrame {
         //jLists configuration
         ingredientRequestJList.setPreferredSize(new Dimension(SCREEN_WIDTH / 3, SCREEN_HEIGHT / 2));
         ingredientJList.setPreferredSize(new Dimension(SCREEN_WIDTH / 3, SCREEN_HEIGHT / 2));
+        ingredientRequestJList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         //labels configuration
         ingredientLabel.setFont(HEADER_FONT);
         ingredientLabel.setHorizontalAlignment(SwingConstants.CENTER);
@@ -109,7 +131,7 @@ public class MainWindow extends JFrame {
 
     }
 
-    private void setMainWindowPreferences() {
+    private void setWindowPreferences() {
         setContentPane(mainPanel);
         setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -117,19 +139,24 @@ public class MainWindow extends JFrame {
         pack();
     }
 
-    /**
-     * вот так юзать мои ивенты. Они в пакете event.entity
-     */
+    public void addIngredientRequest(IngredientRequest ingredientRequest) {
+        ((DefaultListModel<IngredientRequest>) ingredientRequestListModel)
+            .addElement(ingredientRequest);
+    }
+
+    public void removeIngredientRequest(int index) {
+        ((DefaultListModel<IngredientRequest>) ingredientRequestListModel).remove(index);
+    }
 
 //    @EventListener
 //    public void handleIngrChanged(IngredientChangedEvent event) {
-////        ingredientDefaultListModel.addElement(event.getIngredient());
-//        System.out.println(event.getIngredient());
+//        ingredientDefaultListModel.addElement(event.getChangedIngredient());
+//        System.out.println("IngredientChangedEvent: " + event.getChangedIngredient());
 //    }
 //
 //    @EventListener
-//    public void handleIngrChanged2(NewIngredientRequestEvent event) {
-////        ingredientDefaultListModel.addElement(event.getIngredient());
-//        System.out.println(event.getIngredientRequest());
+//    public void handleNewIngr(NewIngredientEvent event) {
+//        ingredientDefaultListModel.addElement(event.getNewIngredient());
+//        System.out.println("NewIngredientEvent: " + event.getNewIngredient());
 //    }
 }
