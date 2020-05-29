900b89a12eb407f17f7568ecde41e11178114f5d
svku0919 <svkulich@gmail.com>
2020-05-30 02:44:36 +0300

    fix: fix event throwing system

--- storage-app/src/main/java/by/pub/storage/app/ingredient/service/IngredientServiceImpl.java
+++ storage-app/src/main/java/by/pub/storage/app/ingredient/service/IngredientServiceImpl.java
@@ -5,11 +5,12 @@ import by.pub.storage.app.event.publisher.StorageEventPublisher;
 import by.pub.storage.app.ingredient.entity.Ingredient;
 import by.pub.storage.app.ingredient.provider.IngredientProvider;
 import by.pub.storage.app.ingredient.repository.IngredientRepository;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 import lombok.SneakyThrows;
 import org.mockito.Mockito;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class IngredientServiceImpl implements IngredientService {
@@ -19,7 +20,7 @@ public class IngredientServiceImpl implements IngredientService {
     private final StorageEventPublisher publisher;
 
     public IngredientServiceImpl(IngredientRepository ingredientRepository,
-        IngredientProvider ingredientProvider, StorageEventPublisher publisher) {
+                                 IngredientProvider ingredientProvider, StorageEventPublisher publisher) {
         this.ingredientRepository = ingredientRepository;
         this.ingredientProvider = ingredientProvider;
         this.publisher = publisher;
@@ -34,33 +35,29 @@ public class IngredientServiceImpl implements IngredientService {
     @SneakyThrows
     public Ingredient orderIngredient(String name, Long amount) {
         Mockito.when(ingredientProvider.provideIngredient(name, amount))
-            .thenReturn(new Ingredient().setAmount(amount)
-                .setName(name));
+                .thenReturn(new Ingredient().setAmount(amount)
+                        .setName(name));
 
         Ingredient ingredientInDb = findIngredientByName(name);
 
         // Imitation of work
         TimeUnit.MILLISECONDS.sleep(100);
         Ingredient orderedIngredient = ingredientProvider.provideIngredient(name, amount);
-        final Ingredient ingredient = saveIngredient(
-            ingredientInDb.setAmount(ingredientInDb.getAmount() + orderedIngredient.getAmount()));
-        publisher.publishEvent(new IngredientChangedEvent(ingredient));
-        return ingredient;
+        return saveIngredient(ingredientInDb.setAmount(ingredientInDb.getAmount() + orderedIngredient.getAmount()));
     }
 
     @Override
     public Ingredient findIngredientByName(String name) {
         return ingredientRepository.findByName(name)
-            .orElseThrow(() -> new RuntimeException("There is no ingredient with name: " + name));
+                .orElseThrow(() -> new RuntimeException("There is no ingredient with name: " + name));
     }
 
     @Override
     public Ingredient findIngredientById(String id) {
         return ingredientRepository.findById(id)
-            .orElseThrow(() -> new RuntimeException("There is no ingredient with id: " + id));
+                .orElseThrow(() -> new RuntimeException("There is no ingredient with id: " + id));
     }
 
-    // TODO: 5/29/20 Fire save event
     @Override
     public Ingredient saveIngredient(Ingredient ingredient) {
         return ingredientRepository.save(ingredient);
@@ -71,17 +68,22 @@ public class IngredientServiceImpl implements IngredientService {
         ingredientRepository.deleteByName(name);
     }
 
-    // TODO: 5/29/20 Delete ingredient when its amount==0
     @Override
     public Ingredient takeIngredientsFromStorage(String ingredientName, Long amount) {
         Ingredient ingredient = findIngredientByName(ingredientName);
+
         if (amount > ingredient.getAmount()) {
             throw new RuntimeException(
-                "There is no enough ingredients on storage. Request some from provider!");
+                    "There is no enough ingredients on storage. Request some from provider!");
         }
 
-        final Ingredient updatedIngredient = ingredientRepository
-            .save(ingredient.setAmount(ingredient.getAmount() - amount));
+        if (amount.equals(ingredient.getAmount())) {
+            deleteIngredientByName(ingredientName);
+            publisher.publishEvent(new IngredientChangedEvent(ingredient.setAmount(0L)));
+            return Ingredient.of(ingredient).setAmount(amount);
+        }
+
+        Ingredient updatedIngredient = ingredientRepository.save(ingredient.setAmount(ingredient.getAmount() - amount));
         publisher.publishEvent(new IngredientChangedEvent(updatedIngredient));
         return Ingredient.of(updatedIngredient).setAmount(amount);
     }
