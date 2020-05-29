bf602b1a7143601581bb897009e0718d7b773953
svku0919 <svkulich@gmail.com>
2020-05-29 13:41:55 +0300

    feat: add bar-app microservice

---
+++ bar-app/src/main/java/by/pub/bar/app/BarAppApplication.java
@@ -0,0 +1,13 @@
+package by.pub.bar.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarAppApplication.class, args);
+    }
+
+}
---
+++ bar-app/src/test/java/by/pub/bar/app/BarAppApplicationTests.java
@@ -0,0 +1,13 @@
+package by.pub.bar.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
