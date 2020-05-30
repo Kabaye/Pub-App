b123c92238926d9d0e1d05a8e3a4efc4a418d5b0
Yauhen Mardan <yauhenmardan@gmail.com>
2020-05-30 21:10:33 +0300

    feat: configure ui

--- bar-app/src/main/java/by/pub/bar/app/ui/table_model/OrderTableModel.java
+++ bar-app/src/main/java/by/pub/bar/app/ui/table_model/OrderTableModel.java
@@ -10,14 +10,25 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderTableModel extends DefaultTableModel {
 
-    private static final Object[] ORDER_TABLE_HEADER = new String[]{"ID", "Name",
-        "Amount", "Status"};
+    private static final Object[] ORDER_TABLE_HEADER = new String[]{"ID", "Client ID",
+        "Price", "Status"};
 
     private List<Order> orders;
 
     public OrderTableModel() {
         super(ORDER_TABLE_HEADER, 0);
         orders = new ArrayList<>();
+    }
+
+    public void addRow(Order order) {
+        super.addRow(
+            new Object[]{order.getId(), order.getClientId(), order.getTotalPrice(),
+                order.getStatus()});
+        orders.add(order);
+    }
+
+    public Order getValueAt(int rowIndex) {
+        return orders.get(rowIndex);
     }
 }
 
