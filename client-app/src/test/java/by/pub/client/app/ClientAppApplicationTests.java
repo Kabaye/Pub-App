0e21df3ebe78aef1e45f05ab6c187dc4ea19dfaf
Kabaye <svkulich@gmail.com>
2020-05-31 00:22:20 +0300

    fix: fix dependencies feat: add client-app

---
+++ client-app/src/test/java/by/pub/client/app/ClientAppApplicationTests.java
@@ -0,0 +1,13 @@
+package by.pub.client.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
